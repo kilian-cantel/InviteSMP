8af11f9b51d113ad4bbcffc78e6fa2efa64c2957
Atzer 722 <kiliancantel@gmail.com>
2025-12-25 03:18:16 +0100

    First commit, init project files.

---
+++ src/main/java/com/atzer/inviteSMP/InviteSMP.java
@@ -0,0 +1,17 @@
+package com.atzer.inviteSMP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InviteSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
