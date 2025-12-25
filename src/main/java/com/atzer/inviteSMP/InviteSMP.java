f9859e6a110d49fe6a558560b72baf5edeac9114
Atzer 722 <kiliancantel@gmail.com>
2025-12-25 03:55:18 +0100

    Add base methods, variables and logic.

--- src/main/java/com/atzer/inviteSMP/InviteSMP.java
+++ src/main/java/com/atzer/inviteSMP/InviteSMP.java
@@ -1,17 +1,39 @@
 package com.atzer.inviteSMP;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InviteSMP extends JavaPlugin {
 
+    private static JavaPlugin instance;
+    private FileConfiguration baseConfig;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        this.saveDefaultConfig();
+        this.baseConfig = this.getConfig();
 
+        this.getLogger().info("Plugin has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        instance = null;
+
+        this.getLogger().info("Plugin has been disabled!");
+    }
+
+    public static JavaPlugin getInstance() {
+        return instance;
+    }
+
+    public FileConfiguration getBaseConfig() {
+        return this.baseConfig;
+    }
+
+    public void reload() {
+        this.reloadConfig();
+        this.baseConfig = this.getConfig();
     }
 }
