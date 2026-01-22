a1aed9830818e03ce53bfcae1659e7c12c83e720
kilian-cantel <kiliancantel@gmail.com>
2026-01-23 00:40:13 +0100

    Add the connect command.

--- src/main/java/com/atzer/inviteSMP/InviteSMP.java
+++ src/main/java/com/atzer/inviteSMP/InviteSMP.java
@@ -1,11 +1,12 @@
 package com.atzer.inviteSMP;
 
+import com.atzer.inviteSMP.command.ConnectCommand;
 import com.atzer.inviteSMP.database.DatabaseManager;
 import com.atzer.inviteSMP.database.repository.PluginPlayerRepository;
 import com.atzer.inviteSMP.listener.PlayerJoinListener;
 import com.atzer.inviteSMP.listener.PlayerMoveListener;
 import com.atzer.inviteSMP.service.PluginPlayerService;
-import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -41,8 +42,19 @@ public final class InviteSMP extends JavaPlugin {
         this.getLogger().info("Database has been loaded!");
 
         //Events
-        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerMoveListener(), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerMoveListener(), this);
+
+        //Commands
+        PluginCommand connectCommand = this.getCommand("connect");
+
+        if (connectCommand == null) {
+            this.getLogger().severe("It's not possible to register the command connect !");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        connectCommand.setExecutor(new ConnectCommand());
 
         this.getLogger().info("Plugin has been enabled!");
     }
---
+++ src/main/java/com/atzer/inviteSMP/command/ConnectCommand.java
@@ -0,0 +1,22 @@
+package com.atzer.inviteSMP.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ConnectCommand implements TabExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        return List.of();
+    }
+}
