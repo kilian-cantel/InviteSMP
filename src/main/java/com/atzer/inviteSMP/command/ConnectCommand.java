83eb0308f51ea32c428a50921cee0a0a47e4f48e
kilian-cantel <kiliancantel@gmail.com>
2026-01-24 00:13:22 +0100

    Add the connect command logic.

--- src/main/java/com/atzer/inviteSMP/command/ConnectCommand.java
+++ src/main/java/com/atzer/inviteSMP/command/ConnectCommand.java
@@ -1,8 +1,13 @@
 package com.atzer.inviteSMP.command;
 
+import com.atzer.inviteSMP.InviteSMP;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,8 +15,21 @@ import java.util.List;
 
 public class ConnectCommand implements TabExecutor {
 
+    private static final InviteSMP plugin = InviteSMP.getInstance();
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("<red>You must be a player to execute this command!"));
+            return false;
+        }
+
+        if (Boolean.TRUE.equals(player.getPersistentDataContainer().get(new NamespacedKey(plugin, "connected"), PersistentDataType.BOOLEAN))) {
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("<red>You are already connected!"));
+            return false;
+        }
+
+        player.getPersistentDataContainer().set(new NamespacedKey(plugin, "connecting"), PersistentDataType.BOOLEAN, true);
         return false;
     }
 
